3f9d87a0771c01621828951bccb4f8a69807a3f2
Nitish kumar <nitishiiitian@gmail.com>
2022-06-09 22:15:13 +0530

    ab

--- backend/app.js
+++ backend/app.js
@@ -31,12 +31,12 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  // For any url we will return this file from backend
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   // For any url we will return this file from backend
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 //Middleware for errors
 app.use(errorMiddleware);
